17d9a56a1e332f684dce4077554cceb7c466bfc5
cox101 <coxb2659@gmail.com>
2024-07-18 13:22:48 -0700

    Initial commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-import signal
 
-# Initialize metrics
-total_file_size = 0
-counter = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
 
 def print_msg(dict_sc, total_file_size):
     """
@@ -26,36 +12,43 @@ def print_msg(dict_sc, total_file_size):
     Returns:
         Nothing
     """
+
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
 
-def signal_handler(sig, frame):
-    """Handle keyboard interruption (CTRL + C) and print metrics."""
-    print_msg(dict_sc, total_file_size)
-    sys.exit(0)
 
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # trimming
-        if len(parsed_line) < 7:
-            continue
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
 
-        counter += 1
+        if len(parsed_line) > 2:
+            counter += 1
 
-        total_file_size += int(parsed_line[-1])  # file size
-        code = parsed_line[-2]  # status code
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
 
-        if code in dict_sc:
-            dict_sc[code] += 1
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-        if counter == 10:
-            print_msg(dict_sc, total_file_size)
-            counter = 0
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 finally:
     print_msg(dict_sc, total_file_size)
